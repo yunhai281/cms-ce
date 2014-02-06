6ebc77d52b11c0d5739d3908c193491094a3e60a
Sten Roger Sandvik <srs@enonic.com>
2014-02-06 12:06:51 +0100

    CMS-2938 Spring xsd is not loaded from jar file in plugins.

--- modules/cms-core/src/main/java/com/enonic/cms/core/plugin/spring/XmlAppContext.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/plugin/spring/XmlAppContext.java
@@ -5,41 +5,17 @@
 
 package com.enonic.cms.core.plugin.spring;
 
-import java.io.IOException;
-
 import org.osgi.framework.Bundle;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.osgi.io.OsgiBundleResourcePatternResolver;
+import org.springframework.osgi.context.support.OsgiBundleXmlApplicationContext;
 
 final class XmlAppContext
-    extends ClassPathXmlApplicationContext
+    extends OsgiBundleXmlApplicationContext
 {
-    private ResourcePatternResolver resolver;
-
     public XmlAppContext( final Bundle bundle )
     {
         setClassLoader( new BundleClassLoader( bundle ) );
-        setConfigLocation("classpath:META-INF/spring/context.xml");
-        setResourcePatternResolver(new OsgiBundleResourcePatternResolver(bundle));
-    }
-
-    public void setResourcePatternResolver(final ResourcePatternResolver resolver)
-    {
-        this.resolver = resolver;
-    }
-
-    @Override
-    public Resource getResource(final String location)
-    {
-        return this.resolver.getResource(location);
-    }
-
-    @Override
-    public Resource[] getResources(final String locationPattern)
-        throws IOException
-    {
-        return this.resolver.getResources(locationPattern);
+        setConfigLocations( new String[]{"META-INF/spring/context.xml"} );
+        setBundleContext( bundle.getBundleContext() );
+        setPublishContextAsService( false );
     }
 }
