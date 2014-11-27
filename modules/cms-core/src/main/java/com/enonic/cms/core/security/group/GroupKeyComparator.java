393adb6dec29dd4de647934518c4d55b6ea6c3c1
Jørgen Sivesind <jsi@enonic.com>
2014-11-27 15:41:05 +0100

    CMS-4530 : GroupKeyComparator class is not serializable - Made it serializable

--- modules/cms-core/src/main/java/com/enonic/cms/core/security/group/GroupKeyComparator.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/security/group/GroupKeyComparator.java
@@ -4,14 +4,17 @@
  */
 package com.enonic.cms.core.security.group;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 /**
  * Jul 20, 2009
  */
 public class GroupKeyComparator
-    implements Comparator<GroupKey>
+    implements Comparator<GroupKey>, Serializable
 {
+    private static final long serialVersionUID = -4975005262376537140L;
+
     public int compare( GroupKey a, GroupKey b )
     {
         return a.toString().compareTo( b.toString() );
