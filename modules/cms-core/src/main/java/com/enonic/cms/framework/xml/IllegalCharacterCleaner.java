fe902657dad9d5de0e2aa28f3e4faef9d64640b4
Runar Myklebust <rmy@enonic.com>
2015-03-18 14:01:20 +0100

    CMS-4199 Indexing stores characters that admin console can not show
    The cleaning of extracted binary data was missing a lot of ASCII control characters. Added.

--- modules/cms-core/src/main/java/com/enonic/cms/framework/xml/IllegalCharacterCleaner.java
+++ modules/cms-core/src/main/java/com/enonic/cms/framework/xml/IllegalCharacterCleaner.java
@@ -8,11 +8,71 @@ import java.util.Arrays;
 
 public final class IllegalCharacterCleaner
 {
-
     // All ASCII controll characters and some other selected special characters.
 
-    private static final String CHARS_TO_REPLACE = "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u000b\u000c\u000e" +
-        "\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f";
+    private static final String CHARS_TO_REPLACE = "\u0000" +
+        "\u0001" +
+        "\u0002" +
+        "\u0003" +
+        "\u0004" +
+        "\u0005" +
+        "\u0006" +
+        "\u0007" +
+        "\u0008" +
+        "\u0009" +
+        "\u000B" +
+        "\u000C" +
+        "\u000E" +
+        "\u000F" +
+        "\u0010" +
+        "\u0011" +
+        "\u0012" +
+        "\u0013" +
+        "\u0014" +
+        "\u0015" +
+        "\u0016" +
+        "\u0017" +
+        "\u0018" +
+        "\u0019" +
+        "\u001A" +
+        "\u001B" +
+        "\u001C" +
+        "\u001D" +
+        "\u001E" +
+        "\u001F" +
+        "\u007F" +
+        "\u0080" +
+        "\u0081" +
+        "\u0082" +
+        "\u0083" +
+        "\u0084" +
+        "\u0085" +
+        "\u0086" +
+        "\u0087" +
+        "\u0088" +
+        "\u0089" +
+        "\u008A" +
+        "\u008B" +
+        "\u008C" +
+        "\u008D" +
+        "\u008E" +
+        "\u008F" +
+        "\u0090" +
+        "\u0091" +
+        "\u0092" +
+        "\u0093" +
+        "\u0094" +
+        "\u0095" +
+        "\u0096" +
+        "\u0097" +
+        "\u0098" +
+        "\u0099" +
+        "\u009A" +
+        "\u009B" +
+        "\u009C" +
+        "\u009D" +
+        "\u009E" +
+        "\u009F";
 
     private static final String CHARS_TO_REPLACE_WITH = " ";
 
