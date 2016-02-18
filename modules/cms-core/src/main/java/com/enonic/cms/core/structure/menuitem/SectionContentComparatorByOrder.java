d77128ac80c466aef730dfd3b24d54a225ebd6e9
Jørgen Sivesind <jsi@enonic.com>
2016-02-18 15:55:17 +0100

    CMS-5382 : NotSerializableExceptions i loggen - Fixed, with some code clean-up.

--- modules/cms-core/src/main/java/com/enonic/cms/core/structure/menuitem/SectionContentComparatorByOrder.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/structure/menuitem/SectionContentComparatorByOrder.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2000-2013 Enonic AS
+ * Copyright 2000-2016 Enonic AS
  * http://www.enonic.com/license
  */
 
 package com.enonic.cms.core.structure.menuitem;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 import com.enonic.cms.core.structure.menuitem.section.SectionContentEntity;
 
 public class SectionContentComparatorByOrder
-    implements Comparator<SectionContentEntity>
+    implements Comparator<SectionContentEntity>, Serializable
 {
+    private static final long serialVersionUID = -5900433713451329377L;
+
     public int compare( SectionContentEntity a, SectionContentEntity b )
     {
         if (a == b) {
@@ -28,18 +31,7 @@ public class SectionContentComparatorByOrder
         }
         else if ( !a.isApproved() && !b.isApproved() )
         {
-            if ( a.getKey().toInt() < b.getKey().toInt() )
-            {
-                return -1;
-            }
-            else if ( a.getKey().toInt() == b.getKey().toInt() )
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
+            return keyCompare( a, b );
         }
         else if ( a.getOrder() < b.getOrder() )
         {
@@ -47,18 +39,23 @@ public class SectionContentComparatorByOrder
         }
         else if ( a.getOrder() == b.getOrder() )
         {
-            if ( a.getKey().toInt() < b.getKey().toInt() )
-            {
-                return -1;
-            }
-            else if ( a.getKey().toInt() == b.getKey().toInt() )
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
+            return keyCompare( a, b );
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    private int keyCompare( final SectionContentEntity a, final SectionContentEntity b )
+    {
+        if ( a.getKey().toInt() < b.getKey().toInt() )
+        {
+            return -1;
+        }
+        else if ( a.getKey().toInt() == b.getKey().toInt() )
+        {
+            return 0;
         }
         else
         {
