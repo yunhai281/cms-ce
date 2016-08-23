5f15cba12fb5fbc09789e55af25a825840b41cc2
Jørgen Sivesind <jsi@enonic.com>
2016-08-23 17:35:42 +0200

    CMS-5420 : Content type fields with regex are always required in admin - Added an exception to the validation for fields that are not mandatory, if the value is empty.  Also found another bug:  When searching through the fields for configs, no effort is made to recurse into block groups.  This is now fixed, so that all TextDataEntries are found.

--- modules/cms-core/src/main/java/com/enonic/cms/core/content/ContentRegExpValidator.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/content/ContentRegExpValidator.java
@@ -2,10 +2,14 @@ package com.enonic.cms.core.content;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.enonic.cms.core.content.contentdata.ContentData;
 import com.enonic.cms.core.content.contentdata.custom.CustomContentData;
 import com.enonic.cms.core.content.contentdata.custom.DataEntry;
+import com.enonic.cms.core.content.contentdata.custom.GroupDataEntry;
 import com.enonic.cms.core.content.contentdata.custom.stringbased.TextDataEntry;
+import com.enonic.cms.core.content.contenttype.ContentTypeConfig;
 import com.enonic.cms.core.content.contenttype.dataentryconfig.DataEntryConfig;
 import com.enonic.cms.core.content.contenttype.dataentryconfig.TextDataEntryConfig;
 
@@ -18,28 +22,42 @@ public class ContentRegExpValidator
         {
             CustomContentData ccData = (CustomContentData) contentData;
 
-            final List<DataEntryConfig> inputConfigs = ccData.getContentTypeConfig().getInputConfigs();
-            for ( DataEntryConfig inputConfig : inputConfigs )
+            final List<DataEntry> dataEntries = ccData.getEntries();
+            validateDataEntries( ccData.getContentTypeConfig(), dataEntries );
+        }
+    }
+
+    private static void validateDataEntries( final ContentTypeConfig contentTypeConfig, final List<DataEntry> dataEntries )
+    {
+        for ( DataEntry dataEntry : dataEntries )
+        {
+            if ( dataEntry instanceof TextDataEntry )
             {
+                final TextDataEntry textDataEntry = (TextDataEntry) dataEntry;
+                String textDataEntryName = textDataEntry.getName();
+                DataEntryConfig inputConfig = contentTypeConfig.getInputConfig( textDataEntryName );
                 if ( inputConfig instanceof TextDataEntryConfig )
                 {
                     String regExp = ( (TextDataEntryConfig) inputConfig ).getRegExp();
-                    if ( ( regExp != null ) && ( !regExp.equals( "" ) ) )
+
+                    if ( StringUtils.isNotEmpty( regExp ) )
                     {
-                        String name = inputConfig.getName();
-                        DataEntry dataEntry = ccData.getEntry( name );
-                        if (!(dataEntry instanceof TextDataEntry)) {
-                            throw new IllegalArgumentException( "The input field, " + name + " is expected to be a text input, but is not." );
-                        }
-                        TextDataEntry textDataEntry = (TextDataEntry) dataEntry;
                         String value = textDataEntry.getValue();
-                        if ( !value.isEmpty() && !value.matches( regExp ) )
+                        Boolean isRequired = inputConfig.isRequired();
+
+                        if ( ( isRequired || StringUtils.isNotEmpty( value ) ) && !value.matches( regExp ) )
                         {
                             throw new IllegalArgumentException(
-                                "The value: " + value + ", does not match the validation reg exp: " + regExp );
+                                "The value: " + textDataEntry.getValue() + ", does not match the validation reg exp: " + regExp );
                         }
                     }
                 }
+            }
+
+            if ( dataEntry instanceof GroupDataEntry )
+            {
+                final GroupDataEntry groupDataEntry = (GroupDataEntry) dataEntry;
+                validateDataEntries( contentTypeConfig, groupDataEntry.getEntries() );
             }
         }
     }
